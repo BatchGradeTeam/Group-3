21d5723608a2b5a0359fcdfecd2f9cbfd2149adf
Tri-T <tranmaitri.244@gmail.com>
2026-02-15 22:40:26 -0800

    Add test create a new counter POST

--- tdd_lab/tests/test_counter.py
+++ tdd_lab/tests/test_counter.py
@@ -10,3 +10,31 @@ how to call the web service and assert what it should return.
 - The service must be able to update a counter by name.
 - The service must be able to read the counter
 """
+
+import pytest
+from src import app
+from src import status
+
+@pytest.fixture()
+def client():
+    """Fixture for Flask test client"""
+    return app.test_client()
+
+@pytest.mark.usefixtures("client")
+class TestCounterEndpoints:
+    """Test cases for Counter API"""
+
+    def test_create_new_counter(self, client):
+        """POST /counters/<name> should create a counter with initial value 0"""
+
+        # send a POST request to create a new counter named "foo"
+        result = client.post("/counters/foo")
+
+        # 1. Verify that the status code is 201 Created
+        assert result.status_code == status.HTTP_201_CREATED
+
+        # 2. Verify that the result contains the counter name and initial value
+        data = result.get_json()    # convert from JSON to a dictionary
+        assert data["name"] == "foo"
+        assert data["value"] == 0
+
